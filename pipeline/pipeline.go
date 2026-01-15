fac3e0ca2008e0e5dd74ac5abaf322da1a59f0c2
valkyraycho <razor0424@gmail.com>
2026-01-15 15:10:43 +0800

    [feat] implement source worker

--- pipeline/pipeline.go
+++ pipeline/pipeline.go
@@ -1,5 +1,10 @@
 package pipeline
 
+import (
+	"context"
+	"fmt"
+)
+
 var _ StageParams = (*workerParams)(nil)
 
 type workerParams struct {
@@ -15,6 +20,22 @@ func (p *workerParams) StageIndex() int { return p.stage }
 func (p *workerParams) Input() <-chan Payload  { return p.inCh }
 func (p *workerParams) Output() chan<- Payload { return p.outCh }
 func (p *workerParams) Error() chan<- error    { return p.errCh }
+
+func sourceWorker(ctx context.Context, source Source, outCh chan<- Payload, errCh chan<- error) {
+	for source.Next(ctx) {
+		payload := source.Payload()
+		select {
+		case outCh <- payload:
+		case <-ctx.Done():
+			return
+		}
+	}
+
+	if err := source.Error(); err != nil {
+		wrappedErr := fmt.Errorf("pipeline source: %w", err)
+		maybeEmitError(wrappedErr, errCh)
+	}
+}
 
 func maybeEmitError(err error, errCh chan<- error) {
 	select {
