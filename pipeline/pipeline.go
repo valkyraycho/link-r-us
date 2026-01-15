4993b272fd424bb104bae5803c2a163fa87a2dbd
valkyraycho <razor0424@gmail.com>
2026-01-15 15:21:40 +0800

    [feat] implement sink worker

--- pipeline/pipeline.go
+++ pipeline/pipeline.go
@@ -21,6 +21,9 @@ func (p *workerParams) Input() <-chan Payload  { return p.inCh }
 func (p *workerParams) Output() chan<- Payload { return p.outCh }
 func (p *workerParams) Error() chan<- error    { return p.errCh }
 
+// sourceWorker implements a worker that reads Payload instances from a Source
+// and pushes them to an output channel that is used as input for the first
+// stage of the pipeline.
 func sourceWorker(ctx context.Context, source Source, outCh chan<- Payload, errCh chan<- error) {
 	for source.Next(ctx) {
 		payload := source.Payload()
@@ -34,6 +37,28 @@ func sourceWorker(ctx context.Context, source Source, outCh chan<- Payload, errC
 	if err := source.Error(); err != nil {
 		wrappedErr := fmt.Errorf("pipeline source: %w", err)
 		maybeEmitError(wrappedErr, errCh)
+	}
+}
+
+// sinkWorker implements a worker that reads Payload instances from an input
+// channel (the output of the last pipeline stage) and passes them to the
+// provided sink.
+func sinkWorker(ctx context.Context, sink Sink, inCh <-chan Payload, errCh chan<- error) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case payload, ok := <-inCh:
+			if !ok {
+				return
+			}
+
+			if err := sink.Consume(ctx, payload); err != nil {
+				wrappedErr := fmt.Errorf("pipeline sink: %w", err)
+				maybeEmitError(wrappedErr, errCh)
+				return
+			}
+		}
 	}
 }
 
